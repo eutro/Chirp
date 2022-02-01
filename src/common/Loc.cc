64b576e81da1ddd8aab54742984ebd2484419e3b
eutro <benedek.szilvasy@gmail.com>
2022-02-01 12:21:06 +0000

    Make SrcLoc col be based on code points rather than bytes

--- src/common/Loc.cc
+++ src/common/Loc.cc
@@ -8,11 +8,28 @@ namespace loc {
     return os;
   }
   void SrcLoc::add(const std::string &s) {
-    for (char c : s) {
+    for (auto it = s.begin(); it != s.end(); ++it) {
+      char c = *it;
       if (c == '\n') {
         ++line;
         col = 0;
       } else {
+        // Count UTF-8 code points
+        if ((c & 0b10000000) != 0b00000000) {
+          unsigned int len;
+          if ((c & 0b11100000) == 0b11000000) {
+            len = 2;
+          } else if ((c & 0b11110000) == 0b11100000) {
+            len = 3;
+          } else if ((c & 0b11111000) == 0b11110000) {
+            len = 4;
+          } else {
+            // invalid UTF-8
+            len = 1;
+          }
+          while (len-- && ++it != s.end());
+          --it;
+        }
         ++col;
       }
     }
@@ -34,7 +51,7 @@ namespace loc {
     return !(*this < rhs);
   }
 
-  Span::Span() {}
+  Span::Span() = default;
   Span::Span(const loc::SrcLoc &lo, const loc::SrcLoc &hi) :
       lo(lo), hi(hi) {}
 }
